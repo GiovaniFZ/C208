0f370135bb49481cad6808d568200260f8272cdd
Giv <giovanifinazzi@gmail.com>
2023-08-16 18:14:49 -0300

    Initial commit

---
+++ mips1.asm
@@ -0,0 +1,4 @@
+#Criando uma variavel ascii
+.data 
+nome: .asciiz "Giv"
+escola: .ascii "Humano"
---
+++ mips2.asm
@@ -0,0 +1,6 @@
+.data 0x10010008
+idade: .byte 0x14 #guarda hexa
+num: .half 0x8090
+num2: .half 0x1122
+num_X: .space 5
+cep: .word 0x40506070
